5816dbc4c66ff942d12f21d34fb0069964b9885d
SvWer <svenwernikowski@yahoo.de>
2021-07-06 14:16:36 +0200

    Now the problem with different length of data and labels is solved

--- dashboard.php
+++ dashboard.php
@@ -73,33 +73,35 @@ foreach ($results as $tuple) {
         'nome' => $tuple->firstname.' '.$tuple->lastname,
         'email' => $tuple->email);
     if ($tuple->week > $maxnumberofweeks) {
-        $maxnumberofweeks = $tuple->week;
+        $maxnumberofweeks = $tuple->week+1;
     }
 }
 
 // Chart with access to course per week
-$access_weeks = array_fill('0', $maxnumberofweeks, 0);
+$access_weeks = array_fill('0', $maxnumberofweeks+1, 0);
 $access_weeks_label = range('0', $maxnumberofweeks);
 foreach ($results as $tuple) {
     if($tuple->userid === $USER->id) {
         $access_weeks[$tuple->week] = intval($tuple->number);
     }
 }
+
 $access_weeks_chart = new core\chart_line();
-$numbers = new core\chart_series('pageviews', $access_weeks);
+$numbers = new core\chart_series('Anzahl der Kursaufrufe pro Woche', $access_weeks);
 $access_weeks_chart->add_series($numbers);
 $access_weeks_chart->set_labels($access_weeks_label);
 
 //Chart with klicks in course per week
-$klicks = array_fill('0', $maxnumberofweeks, 0);
+$klicks = array_fill('0', $maxnumberofweeks+1, 0);
 $klick_label = range('0', $maxnumberofweeks);
 foreach ($results as $tuple) {
     if($tuple->userid === $USER->id) {
         $klicks[$tuple->week] = intval($tuple->numberofpageviews);
     }
 }
+
 $klicks_chart = new core\chart_line();
-$klicks_s = new core\chart_series('pageviews', $klicks);
+$klicks_s = new core\chart_series('Anzahl der Klicks im Kurs pro Woche', $klicks);
 $klicks_chart->add_series($klicks_s);
 $klicks_chart->set_labels($klick_label);
 
@@ -107,21 +109,21 @@ $klicks_chart->set_labels($klick_label);
 $accessresults = block_disea_dashboard_get_number_of_modules_access_by_week($courseid, $students, $startdate);
 
 // Chart with days of access of modules per week
-$module_access = array_fill('0', $maxnumberofweeks, 0);
-$module_access_label = range('0', $maxnumberofweeks-1);
+$module_access = array_fill('0', $maxnumberofweeks+1, 0);
+$module_access_label = range('0', $maxnumberofweeks);
 foreach ($accessresults as $tuple) {
     if($tuple->userid === $USER->id) {
         $module_access[$tuple->week] = intval($tuple->number);
     }
 }
+
 $module_access_chart = new core\chart_line();
-$module_access_s = new core\chart_series('module views', $module_access);
+$module_access_s = new core\chart_series('Anzahl aufgerufener Aktivitaeten pro Woche', $module_access);
 $module_access_chart->add_series($module_access_s);
 $module_access_chart->set_labels($module_access_label);
 
-
 //Testchart 3 just for fun
-$nums = new core\chart_series('numbers2', [1, 2, 6, 4, 5, 2, 7, 8, 5, 10, 1, 4, 7, 3]);
+$nums = new core\chart_series('Random Stuff', [1, 2, 6, 4, 5, 2, 7, 8, 5, 10, 1, 4, 7, 3]);
 $labs = array('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14');
 $chart4 = new core\chart_line();
 $chart4->add_series($nums);
@@ -141,24 +143,11 @@ $diagrams = [
 
 $templatecontext_diagrams = (object) [
     'diagrams' => $diagrams,
-    'test' => $OUTPUT->render_chart($klicks_chart),
 ];
 
 
 echo $OUTPUT->header();
 echo $OUTPUT->heading('DiSEA Dashboard');
-/*echo '<div class="container"> <div class="row"> <div class="col">';
-echo $OUTPUT->render_chart($access_weeks_chart, false);
-echo '</div>  <div class="col">';
-echo $OUTPUT->render_chart($module_access_chart, false);
-echo '</div>  <div class="col">';
-echo $OUTPUT->render_chart($klicks_chart);
-echo '</div> </div> <div class="row">';
-echo $OUTPUT->render_chart($chart4, false);
-echo '</div> </div> </div>';
-echo '<hr><hr><hr><hr>';
-echo '<h2>Hier folgt ein test</h2>';
-*/
 echo $OUTPUT->render_from_template('block_disea_dashboard/diagrams', $templatecontext_diagrams);
 echo $OUTPUT->render_from_template('block_disea_dashboard/more_details', $templatecontext);
 echo $OUTPUT->footer();
